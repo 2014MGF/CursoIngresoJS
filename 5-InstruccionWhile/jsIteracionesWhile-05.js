cab33fab5bbd111225334eb89252f392326d4771
2014MGF <milciadesgonzalez1920@gmail.com>
2021-07-15 23:15:19 -0300

    parcial

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -6,7 +6,15 @@ function mostrar()
 	var sexoIngresado;
 	sexoIngresado = prompt("ingrese f ó m .");
 
+	while(sexoIngresado != "f" && sexoIngresado != "m"){
+		alert("Error, reintente.");
+		sexoIngresado = prompt("ingrese f ó m .");
+	}
+	if (sexoIngresado == "f") {
+		sexoIngresado = "Femenino";
+	} else {
+		sexoIngresado = "Masculino";
+	}
+	document.getElementById("txtIdSexo").value = sexoIngresado;
 
-
-	txtIdSexo.value=sexoIngresado;
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,44 @@
 function mostrar()
 {
-  alert("dos");
+  let articulo,
+    cantidad,
+		precioArena=40,
+		precioCal=50,
+		precioCemento=60,
+    sumaArena=0,
+    sumaCal=0,
+    sumaCemento=0,
+    tarifaArena,
+    tarifaCal,
+    tarifaCemento,
+    cantidadTotal,
+    totalBruto,
+    totalDescuento,
+		respuesta = "si";
+
+	respuesta = prompt("Quiere ingresar un articulo? si/no");
+
+	while(respuesta == "si"){
+    articulo = prompt("Ingrese articulo.");
+
+    if (articulo == "arena" || articulo == "cal" || articulo == "cemento") {
+      cantidad = parseInt(prompt("Ingrese cantidad."));
+      if (articulo == "arena"){
+        sumaArena += cantidad;
+        tarifaArena = precioArena * sumaArena;
+      } else if (articulo == "cal"){
+        sumaCal += cantidad;
+        tarifaCal = precioCal * sumaCal;
+      } else {
+        sumaCemento += cantidad;
+        tarifaCemento = precioCemento * sumaCemento;
+      }
+    }
+		respuesta = prompt("Quiere seguir ingresando articulos? si/no");
+	}
+  cantidadTotal = sumaArena + sumaCal + sumaCemento;
+  precioTotal = precioArena + precioCal + precioCemento;
+	if (cantidadTotal > 30){
+    totalBruto = precioTotal * 0.75;
+  } else if (){}
 }
