56740d5bc71777d8e8278d9f6d8db3f50e228e2b
2014MGF <milciadesgonzalez1920@gmail.com>
2021-07-22 12:34:54 -0300

    clase 22/7

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,30 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
-function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
-	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+function mostrar() {
+	
+	let numero = 0,
+		maximo,
+		minimo,
+		flag = 0,
+		respuesta;
+
+	do {
+		numero = parseInt(prompt("Ingrese un numero:"));
+		while (isNaN(numero)) {
+			numero = parseInt(prompt("Eso no es un numero, ingrese nuevamente:"));
+		}
+		if (flag == 0) {
+			minimo = numero;
+			maximo = numero;
+			flag = 1
+		} else if (minimo > numero){
+			minimo = numero;
+		} else if (maximo < numero){
+			maximo = numero;
+		}
+		respuesta = prompt("Quiere ingresar otro numero? (s/n)").toLowerCase();
+	} while (respuesta == "s");
+	document.getElementById("txtIdMaximo").value = maximo;
+	document.getElementById("txtIdMinimo").value = minimo;
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -10,20 +10,56 @@ hasta que el usuario quiera, mostrar:
 7-Promedio de positivos.
 8-Promedios de negativos.
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
-function mostrar()
-{
-	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+function mostrar() {
+	let numero,
+		acumuladorPositivos = 0,
+		acumuladorNegativos = 0,
+		contadorPositivos = 0,
+		contadorNegativos = 0,
+		contadorCeros = 0,
+		contadorNumPares = 0,
+		promedioPositivos = 0,
+		promedioNegativos = 0,
+		diferencia,
+		respuesta;
 
-	respuesta="si";
 
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+	do {
+		numero = parseInt(prompt("Ingrese un número:"));
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+		while (isNaN(numero)) {
+			numero = parseInt(prompt("Eso no es un número. Reingrese número:"));
+		}
+		if (numero > 0) {
+			acumuladorPositivos += numero;
+			contadorPositivos++;
+		} else if (numero < 0) {
+			acumuladorNegativos += numero;
+			contadorNegativos++;
+		} else {
+			contadorCeros++;
+		}
+		if (numero % 2 == 0) {
+			contadorNumPares++;
+		}
+		respuesta = prompt("Quiere seguir ingresando números? s/n").toLowerCase();
+	} while (respuesta == `s`);
+	if (contadorPositivos != 0) {
+		promedioPositivos = acumuladorPositivos/contadorPositivos;
+	}
+	if (contadorNegativos != 0){
+		promedioNegativos = acumuladorNegativos/contadorNegativos;
+	}
+	diferencia = contadorPositivos - contadorNegativos;
+
+	console.log("1-Suma de los negativos."+acumuladorNegativos);
+	console.log("2-Suma de los positivos."+acumuladorPositivos);
+	console.log("3-Cantidad de positivos."+contadorPositivos);
+	console.log("4-Cantidad de negativos."+contadorNegativos);
+	console.log("5-Cantidad de ceros."+contadorCeros);
+	console.log("6-Cantidad de números pares."+contadorNumPares);
+	console.log("7-Promedio de positivos."+promedioPositivos);
+	console.log("8-Promedios de negativos."+promedioNegativos);
+	console.log("9-Diferencia entre positivos y negativos."+diferencia);
+
 }//FIN DE LA FUNCIÓN
